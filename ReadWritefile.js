4354cbb97f07c6bee71c17c9d93f37d3ef893067
Phamminhpv <48233891+Phamminhpv@users.noreply.github.com>
2019-04-04 22:49:11 +0700

    Update ReadWritefile.js

--- ReadWritefile.js
+++ ReadWritefile.js
@@ -3,8 +3,10 @@ const fs = require ('fs');
 const wf = (file, data) => {
     return new Promise((resolve, reject) => {
         fs.writeFile(file,data,(err) => {
-            if(err) reject(console.log(err));
-            resolve (console.log('file saved'));
+            if(err) 
+                reject(err); 
+            } else{
+                resolve ('file saved');
 
         })
     })
@@ -13,8 +15,10 @@ const wf = (file, data) => {
 const rf = (file) => {
     return new Promise((resolve, reject) => {
         fs.readFile(file,'utf8',(err, data) => {
-            if (err) reject(err)
-            resolve (console.log(data));
+            if(err) 
+                reject(err);
+            } else{
+                resolve (data);
 
         })
     })
